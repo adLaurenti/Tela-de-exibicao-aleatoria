0df1ee846e16213663ec5bd4c11b49930a519660
adLaurenti <a-laurenti@hotmail.com>
2022-01-16 15:43:11 -0300

    atualização de pagina

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
 
+TraduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não Sabemos'; 
+    }else if(data.status == 'Alive') {
+        return 'Sim';
+    } else {
+        return 'Não. Está morto'
+    }
+    }
+
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 3);
 }
